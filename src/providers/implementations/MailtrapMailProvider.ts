fc9db5275d61fda33ad7e72868c8213a88e3fb9b
Gustavo-DelColi <gustavodelcoli@hotmail.com>
2024-06-28 18:04:10 -0300

    start

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,34 @@
+import Mail from "nodemailer/lib/mailer";
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer'
+
+export class MailtrapMailProvider implements IMailProvider{
+    private transporter: Mail;
+
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+          host:'smtp.mailtrap.io',
+          port: 2525,
+          auth:{
+            user: 'd719a721d085cc',
+            pass: '7869eff5b4fb66'
+          }  
+        })
+    }
+
+    async sendMail(message: IMessage): Promise<void>{
+        await this. transporter.sendMail({
+            to: {
+                name: message.to.email,
+                address: message.to.email,
+            },
+            from: {
+                name: message.from.email,
+                address: message.from.email,
+            },
+            subject: message.subject,
+            html: message.body,
+        })
+    }
+
+}
